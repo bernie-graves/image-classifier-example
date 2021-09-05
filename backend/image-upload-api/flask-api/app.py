e04ceac4ce8c389d33e22bd7f4c1ed78eec13ce7
bernie-graves <75196548+bernie-graves@users.noreply.github.com>
2021-09-05 15:57:11 -0700

    Served index.html on flask

--- backend/image-upload-api/flask-api/app.py
+++ backend/image-upload-api/flask-api/app.py
@@ -1,5 +1,6 @@
 from flask import Flask
 
-app = Flask(__name__)
+app = Flask(__name__, static_url_path='',
+            root_path='../image-classifier-example/', static_folder='frontend')
 app.secret_key = "secret key"
 app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024
--- backend/image-upload-api/flask-api/main.py
+++ backend/image-upload-api/flask-api/main.py
@@ -1,34 +1,42 @@
 
 from app import app
-from flask import Flask, request, redirect, jsonify
+from flask import Flask, request, redirect, jsonify, send_from_directory
 
 
 # Allowed file types
-ALLOWED_EXTENSIONS = set(['jpeg','png','jpg'])
+ALLOWED_EXTENSIONS = set(['jpeg', 'png', 'jpg'])
+
 
 # File extension must be from ALLOWED_EXTENSIONS
 def allowed_file(filename):
     return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+
+# serving index.html on flask
+@app.route('/')  # routing it to the home page
+def home():
+    return send_from_directory('frontend', 'index.html')
+
 
 # API to validate the Image file
 @app.route('/image-upload', methods=["POST"])
 def image_upload():
     # Check if the post request has the file part
     if 'file' not in request.files:
-        resp = jsonify({'message' : 'No file part in the request'})
+        resp = jsonify({'message': 'No file part in the request'})
         resp.status_code = 400
         return resp
 
     file = request.files['file']
     # Check if a file was selected in the request
-    if file.filename =='':
+    if file.filename == '':
         resp = jsonify({'message': "No Image selected for upload"})
         resp.status_code = 400
         return resp
 
     # Correct request
     if file and allowed_file(file.filename):
-        resp = jsonify({'message' : 'Success!'})
+        resp = jsonify({'message': 'Success!'})
         resp.status_code = 201
         return resp
 
@@ -36,6 +44,7 @@ def image_upload():
         resp = jsonify({'message': 'Allowed file types are jpeg, png, jpg'})
         resp.status_code = 400
         return resp
+
 
 # Start Flask App
 if __name__ == "__main__":
