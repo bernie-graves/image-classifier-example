5ea9e89f13db9a6fd41db702aad3a089e6acea04
Yimin Arava <85930454+YiminArava4508@users.noreply.github.com>
2021-09-09 22:24:41 -0700

    Deserialize model object
    the API should now deserialize the model object and transform the user's image into a numpy array so the model can predict a response

--- backend/image-upload-api/flask-api/main.py
+++ backend/image-upload-api/flask-api/main.py
@@ -1,36 +1,57 @@
-# Imports
+# Imports ------------------------------------------------------------------------------------------------------------
 from app import app
 from flask import Flask, request, redirect, jsonify, send_from_directory
+import tensorflow as tf
+from tensorflow import keras
+import numpy as np
+import json
+from PIL import Image,  ImageOps
+from tensorflow.keras.models import model_from_json
 
-import pickle
-import os
-
-# Allowed file types
+# Allowed file types for the 
 ALLOWED_EXTENSIONS = set(['jpeg', 'png', 'jpg'])
-
-
 # File extension must be from ALLOWED_EXTENSIONS
 def allowed_file(filename):
     return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
 
-# serving index.html on flask
+# Deserialize the json file and create the model-----------------------------------------------------------------------
+with open("./notebooks/models/2021-09-06-keras.json", "r") as json_file:
+    json_model = json.load(json_file)
+
+model_test = model_from_json(json_model)
+
+# Some functions for resp ---------------------------------------------------------------------------------------------
+def result(array):
+    if array[0][0] > array[0][1]:
+        return 'genuine'
+    elif array[0][0] < array[0][1]:
+        return 'forged'
+    else:
+        return 'unable to determine'
+def result_percentage(array):
+    if array[0][0] > array[0][1]:
+        return "{:.2f}".format(array[0][0] * 100)
+    elif array[0][0] < array[0][1]:
+        return "{:.2f}".format(array[0][1] * 100)
+    else:
+        return 'unable to determine'
+# serving index.html on flask -----------------------------------------------------------------------------------------
 @app.route('/')  # routing it to the home page
 def home():
     return send_from_directory('frontend', 'index.html')
 
-
-# API to validate the Image file
+# API to validate the Image file --------------------------------------------------------------------------------------
 @app.route('/image-upload', methods=["POST"])
 def image_upload():
-    # Check if the post request has the file part
+    # Handler for no file part in request
     if 'file' not in request.files:
         resp = jsonify({'message': 'No file part in the request'})
         resp.status_code = 400
         return resp
 
     file = request.files['file']
-    # Check if a file was selected in the request
+    # Handler for no file selected
     if file.filename == '':
         resp = jsonify({'message': "No Image selected for upload"})
         resp.status_code = 400
@@ -38,7 +59,20 @@ def image_upload():
 
     # Correct request
     if file and allowed_file(file.filename):
-        resp = jsonify({'message': 'Success!'})
+
+        img = Image.open(file)
+        if 'L' in img.getbands():
+            img = ImageOps.colorize(img, black='blue', white='white')
+        img_after_resize = img.resize((200,200))
+        img_array = np.asarray(img_after_resize)
+        img_array_optimized = np.array(img_array, dtype = 'float32')/255.0
+        data = img_array_optimized.reshape((1,-1))
+        prediction = model_test.predict(data)
+        print(prediction)
+        resp = jsonify({
+            'predicted-label':  str(result(prediction)),
+            "prediction-confidence-percentage": str(result_percentage(prediction))
+            })        
         resp.status_code = 201
         return resp
 
@@ -46,12 +80,8 @@ def image_upload():
         resp = jsonify({'message': 'Allowed file types are jpeg, png, jpg'})
         resp.status_code = 400
         return resp
-# Deserialization ---------------------------------------------------------------------------------
 
-model = open('./notebooks/models/'+'2021-09-04-SVC.pkl','rb')
-new_dict = pickle.load(model)
-model.close()
-print(new_dict)
+
 
 
 # Start Flask App
